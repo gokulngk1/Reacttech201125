6b7eeea51f03760a841b0c1fed706e54bbab4082
gokulngk1 <gokulngk1@gmail.com>
2025-11-20 16:12:59 +0530

    promise

--- html-css-javascript/Script.js
+++ html-css-javascript/Script.js
@@ -15,17 +15,35 @@ var isRad = true;
 // document.getElementById('box1').innerHTML = number+5;
 
 
-var groceries = ['Milk','Eggs','Cheese'];
-function listGroceries(){
-    for(i=0; i< groceries.length;i++){
-    console.log(groceries[i]);
-}
-}
-listGroceries();
+// var groceries = ['Milk','Eggs','Cheese'];
+// function listGroceries(){
+//     for(i=0; i< groceries.length;i++){
+//     console.log(groceries[i]);
+// }
+// }
+// listGroceries();
 
-document.getElementById('bt').addEventListener('click',function(){
-    alert('successfully submitted')
-}); 
+// document.getElementById('bt').addEventListener('click',function(){
+//     alert('successfully submitted')
+// }); 
+
+// Promise
+
+function getUsers(){
+    return[
+        {username: 'john', email:'john@test.com'},
+        {username: 'jane', email:'jane@test.com'},
+    ]
+}
+
+function findUser(username){
+    const users = getUsers();
+    const user = users.find((user)=> user.username === username);
+    return user;
+}
+
+console.log(findUser('john'));
+
 
 
 
