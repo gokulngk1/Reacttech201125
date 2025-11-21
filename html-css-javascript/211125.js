c9e7f6b66e7146907f9eda74be3153914cab38c3
gokulngk1 <gokulngk1@gmail.com>
2025-11-21 14:13:05 +0530

    21/11/25 pratice

---
+++ html-css-javascript/211125.js
@@ -0,0 +1,138 @@
+// //arrays 
+
+// let num = 10;
+// let name = "Gokul";
+
+
+// let cities = ["chennai", "Madurai","Trichy"];
+
+// let marks = [78, 23, 4, 23, 34 ];
+
+// // length - total element in the array 
+
+// // console.log(cities.length);
+// // console.log(marks.length);
+
+// // console.log(cities[0]);
+// // console.log(cities[1]);
+// // console.log(cities[2]);
+
+// // last element 
+
+// // console.log(marks[marks.length-1]);
+// // console.log(marks[marks.length-2]);
+// // console.log(marks[marks.length-3]);
+
+// // mix of int and string 
+
+// // let arr = [3, 4, 5, 6, 'a', "abc",[3,4]];
+
+// // console.log(arr);
+// // console.log(arr[6][0]);
+// // console.log(arr[6][1]);
+
+
+// //2d array 
+
+// // let matrix = [[3, 4, 5], [4, 5, 7], [6, 7, 8]];
+
+// // console.log(matrix[0]);
+// // console.log(matrix[1]);
+// // console.log(matrix[2]);
+
+// // console.log(matrix[0][0]);
+// // console.log(matrix[0][1]);
+// // console.log(matrix[0][2]);
+
+// // console.log(matrix[1][0]);
+// // console.log(matrix[1][1]);
+// // console.log(matrix[1][2]);
+
+// // console.log(matrix[2][0]);
+// // console.log(matrix[2][1]);
+// // console.log(matrix[2][2]);
+
+// // Array Methods 
+
+// let array = ['a', 'b', 'c', 'd', 'e'];
+
+// // push - add element to the end 
+
+// array.push('f')
+
+// console.log(array.pop());
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array.unshift('a'));
+// console.log(array);
+
+// // delete array[2]
+// // console.log(array);
+
+// // array.splice(2,2);
+// // array.splice(0,1);
+// console.log(array);
+
+// array.splice(1,1,'m')
+// console.log(array);
+
+// console.log(array.slice(1,3));
+// console.log(array.slice(1,2));
+
+// array.reverse()
+// console.log(array);
+
+// let str = array.join()
+// console.log(str);
+
+// let str3 = "g,t,r,e"
+// let arr3 = str3.split(',')
+// console.log(arr3);
+
+
+// let nums = [1, 2, 3];
+// let doubled = nums.map(n => n*2);
+
+// console.log(doubled);
+
+// let firstArr = [1, 2, 3];
+// let secoundArr = [1, 2, 3];
+
+// let joinedArr = [firstArr,secoundArr];
+// console.log(joinedArr)
+
+// joinedArr = firstArr.concat(secoundArr)
+// console.log(joinedArr)
+
+// let joined = [...firstArr, ...secoundArr];
+// console.log(joinedArr)
+
+
+// // for loop 
+
+// let Arr = [12, 10, 23];
+
+// for(let i=0; i<Arr.length; i++){
+//     // console.log(Arr[i]);
+    
+// } 
+
+// for(let value of Arr){
+//   console.log(value);
+// }
+
+// Arr.forEach(item => {
+//   console.log(item);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
